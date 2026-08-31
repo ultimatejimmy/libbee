19b61ac9dc7b81b2fd041667fd2de2881ad68301
Jimmy Pautz <jpautz@watermarkinsights.com>
2026-08-31 08:23:51 -0500

    Release 26.8.31-beta

--- libbee.koplugin/_meta.lua
+++ libbee.koplugin/_meta.lua
@@ -13,7 +13,7 @@ return {
     name        = "libbee",
     fullname    = _("menu_libbee"),
     description = _("menu_libbee_desc"),
-    version = "26.8.27",
+    version = "26.8.31-beta",
     author      = "ultimatejimmy",
 }
 
